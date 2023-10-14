303c31df99de6616efcda088141ad15c2b909f0d
Ian McClue <ian@nocta.ai>
2023-10-14 12:38:43 +0100

    Quick updates

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,13 @@
 import type { Site, SocialMediaObjects } from "./types";
 
 export const SITE: Site = {
-    siteUrl: "https:/www.astro-lane.avenuelabs.co/", // Always put "/" at the end of the URL
+    siteUrl: "https:/www.fiatluxlabs.co/", // Always put "/" at the end of the URL
     author: "Ian McClue | Fiat Lux Labs",
-    desc: "A personal portfolio landing template for developers and designers. Made by Fiat Lux Labs.",
-    title: "Astro Lane | Fiat Lux Labs",
+    desc: "A project portfolio made by Fiat Lux Labs.",
+    title: "Fiat Lux Labs | Project Portfolio",
     ogImage: "images/astro-lane.png",
     keywords:
-        "Personal portfolio, landing page, page template, developer portfoliom designer portfolio",
+        "Project portfolio, landing page, Fiat Lux Labs",
     postPerPage: 3
 };
 
@@ -18,77 +18,77 @@ export const SITE: Site = {
 export const SOCIALS: SocialMediaObjects = [
     {
         name: "Github",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "ph:github-logo-duotone",
         title: `Follow ${SITE.author} on Github`,
         active: true
     },
     {
         name: "Facebook",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "ph:facebook-logo-duotone",
         title: `Follow ${SITE.title} on Facebook`,
         active: false
     },
     {
         name: "Instagram",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "ph:instagram-logo-duotone",
         title: `Follow ${SITE.author} on Instagram`,
-        active: true
+        active: false
     },
     {
         name: "LinkedIn",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "ph:linkedin-logo-duotone",
         title: `Follow ${SITE.title} on LinkedIn`,
         active: true
     },
     {
         name: "Mail",
-        href: "mailto:yourmail@gmail.com",
+        href: "mailto:ianmcclue@protonmail.com",
         title: `Send an email to ${SITE.title}`,
         icon: "ph:envelope-simple-bold",
         active: true
     },
     {
         name: "Twitter",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "ph:twitter-logo-duotone",
         title: `Follow ${SITE.author} on Twitter`,
-        active: true
+        active: false
     },
     {
         name: "YouTube",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "",
         title: `${SITE.title} on YouTube`,
         active: false
     },
     {
         name: "WhatsApp",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "",
         title: `${SITE.title} on WhatsApp`,
         active: false
     },
     {
         name: "Snapchat",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "",
         title: `${SITE.title} on Snapchat`,
         active: false
     },
     {
         name: "CodePen",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "",
         title: `${SITE.title} on CodePen`,
         active: false
     },
     {
         name: "Discord",
-        href: "https://github.com/christian-luntok/astro-lane/",
+        href: "https://github.com/Ian-nocta",
         icon: "",
         title: `${SITE.title} on Discord`,
         active: false
